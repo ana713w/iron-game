475d64bf4afaf45221e03c7a7d2f19364589cf3c
Ana Molina <ana713w@gmail.com>
2024-11-02 15:12:49 +0100

    fix: ranking / instructions in button

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,8 +4,16 @@ const restartButton = document.getElementById('button-game-over');
 const gamesIntro = document.getElementById('game-intro');
 const gamesOver = document.getElementById('game-over');
 const formName = document.getElementById('fname');
+const instructionsButton = document.getElementById('button-instructions');
+const instructions = document.getElementById('instructions');
+const rankingButton = document.getElementById('button-ranking');
+const ranking = document.getElementById('ranking');
+const backButtonI = document.getElementById('button-back-i');
+const backButtonR = document.getElementById('button-back-r');
 
 gamesOver.style.visibility = "hidden";
+ranking.style.visibility = "hidden";
+instructions.style.visibility = "hidden";
 
 const ctx = canvas.getContext('2d');
 
@@ -22,10 +30,29 @@ startButton.addEventListener('click', () => {
         game.playerName = formName.value.trim();
         game.start();
     }
-})
+});
 
 restartButton.addEventListener('click', () => {
     window.location.reload();
-})
+});
 
+rankingButton.addEventListener('click', () => {
+    ranking.style.visibility = "visible";
+    gamesIntro.style.visibility = "hidden";
+});
+
+instructionsButton.addEventListener('click', () => {
+    instructions.style.visibility = "visible";
+    gamesIntro.style.visibility = "hidden";
+});
+
+backButtonR.addEventListener('click', () => {
+    ranking.style.visibility = "hidden"
+    gamesIntro.style.visibility = "visible";
+});
+
+backButtonI.addEventListener('click', () => {
+    instructions.style.visibility = "hidden"
+    gamesIntro.style.visibility = "visible";
+});
 
